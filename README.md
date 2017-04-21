6f697cadbb847e7ec756ed55333503ad3bd84273
Santiago Ferreira <san650@gmail.com>
2017-04-21 20:26:04 -0300

    Add confetti component

---
+++ README.md
@@ -0,0 +1,79 @@
+# ember-confetti
+[![Build Status](https://travis-ci.org/san650/ember-confetti.svg?branch=master)](https://travis-ci.org/san650/ember-confetti)
+[![Ember Observer Score](https://emberobserver.com/badges/ember-confetti.svg)](https://emberobserver.com/addons/ember-confetti)
+
+This addon exposes an `{{confetti-rain}}` component for your ember application.
+
+## Usage
+
+Install the addon and insert the component in any template.
+
+```hbs
+{{confetti-rain}}
+```
+
+The component will start a confetti rain when rendered or you can toggle the rain by setting the `isEnabled` attribute.
+
+```hbs
+{{!--
+
+  No confetti rain :-(
+
+--}}
+
+{{confetti-rain isEnabled=false}}
+```
+
+### Overriding z-index
+
+By default `z-index` is set to max 32bit integer value, to override this you can set a custom class to the component and set your desired `z-index` value.
+
+```hbs
+{{confetti-rain class="my-confetti"}}
+```
+
+```css
+.my-confetti {
+  z-index: 10!important;
+}
+```
+
+Note that you need to use the `!important` suffix so the rule is applied correctly.
+
+## Installation
+
+To install the addon just run
+
+```
+$ ember install ember-confetti
+```
+
+## Development
+
+```sh
+$ git clone https://github.com/san650/ember-confetti
+$ cd $_
+$ yarn          # (or npm install)
+$ bower install
+```
+
+Running tests
+
+```sh
+$ npm test
+```
+
+## Project's health
+
+[![Build Status](https://travis-ci.org/san650/ember-confetti.svg?branch=master)](https://travis-ci.org/san650/ember-confetti)
+[![Ember Observer Score](https://emberobserver.com/badges/ember-confetti.svg)](https://emberobserver.com/addons/ember-confetti)
+
+## Acknowledgement
+
+The confetti idea is based on Linmiao Xu's codepen http://codepen.io/linrock/pen/Amdhr
+
+## License
+
+ember-confetti is licensed under the MIT license.
+
+See [LICENSE](./LICENSE) for the full license text.
---
+++ addon/components/confetti-rain.js
@@ -0,0 +1,194 @@
+import Ember from 'ember';
+
+const { computed } = Ember;
+
+const raf = window.requestAnimationFrame ||
+    window.webkitRequestAnimationFrame ||
+    window.mozRequestAnimationFrame ||
+    window.oRequestAnimationFrame ||
+    window.msRequestAnimationFrame ||
+    function (callback) {
+      return window.setTimeout(callback, 1000 / 60);
+    };
+
+const COLORS = [
+  "DodgerBlue",
+  "OliveDrab",
+  "Gold",
+  "pink",
+  "SlateBlue",
+  "lightblue",
+  "Violet",
+  "PaleGreen",
+  "SteelBlue",
+  "SandyBrown",
+  "Chocolate",
+  "Crimson"
+];
+
+const MAX_PARTICLES = 150;
+
+const { random } = Math;
+
+const MAX_ZINDEX = 2147483647;
+
+function randomFromTo(from, to) {
+  return Math.floor(random() * (to - from + 1) + from);
+}
+
+function ConfettiParticle({ color, width, height, ctx }) {
+  this.x = random() * width; // x-coordinate
+  this.y = (random() * height) - height; //y-coordinate
+  this.r = randomFromTo(10, 30); //radius;
+  this.d = (random() * MAX_PARTICLES) + 10; //density;
+  this.color = color;
+  this.tilt = Math.floor(random() * 10) - 10;
+  this.tiltAngleIncremental = (random() * 0.07) + 0.05;
+  this.tiltAngle = 0;
+
+  this.draw = function () {
+    ctx.beginPath();
+    ctx.lineWidth = this.r / 2;
+    ctx.strokeStyle = this.color;
+    ctx.moveTo(this.x + this.tilt + (this.r / 4), this.y);
+    ctx.lineTo(this.x + this.tilt, this.y + this.tilt + (this.r / 4));
+    return ctx.stroke();
+  };
+}
+
+const colorGenerator = {
+  colorIndex: 0,
+  colorIncrementer: -1,
+  colorThreshold: 10,
+  getColor() {
+    this.colorIncrementer = (this.colorIncrementer + 1) % 10;
+
+    if (!this.colorIncrementer) {
+      this.colorIndex = (this.colorIndex + 1) % COLORS.length;
+    }
+
+    return COLORS[this.colorIndex];
+  }
+};
+
+export default Ember.Component.extend({
+  tagName: 'canvas',
+  attributeBindings: ['style'],
+
+  style: computed(function() {
+    return `
+      pointer-events: none;
+      position: fixed;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      left: 0;
+      z-index: ${MAX_ZINDEX};
+      width: 100%;
+      height: 100%;
+    `;
+  }),
+
+  particles: computed(function() {
+    let particles = [];
+
+    for (var i = 0; i < MAX_PARTICLES; i++) {
+      particles.push(new ConfettiParticle({
+        color: colorGenerator.getColor(),
+        width: this.get('windowWidth'),
+        height: this.get('windowHeight'),
+        ctx: this.get('context')
+      }));
+    }
+
+    return particles;
+  }),
+
+  didInsertElement() {
+    this._super(...arguments);
+
+    //canvas init
+    var canvas = this.$().get(0);
+
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    this.setProperties({
+      context: canvas.getContext("2d"),
+      windowWidth: window.innerWidth,
+      windowHeight: window.innerHeight,
+      angle: 0,
+      tiltAngle: 0
+    });
+
+    this.animationLoop();
+  },
+
+  draw() {
+    this.get('context').clearRect(0, 0, this.get('windowWidth'), this.get('windowHeight'));
+
+    var results = this.get('particles').map(p => p.draw());
+
+    this.update();
+
+    return results;
+  },
+
+  update() {
+    var remainingFlakes = 0;
+    var particle;
+    this.incrementProperty('angle', 0.01);
+    this.incrementProperty('tiltAngle', 0.1);
+
+    for (var i = 0; i < MAX_PARTICLES; i++) {
+      particle = this.get('particles')[i];
+
+      this.stepParticle(particle, i);
+
+      if (particle.y <= this.get('windowHeight')) {
+        remainingFlakes++;
+      }
+      this.checkForReposition(particle, i);
+    }
+  },
+
+  checkForReposition(particle, index) {
+    if (particle.x > this.get('windowWidth') + 20 || particle.x < -20 || particle.y > this.get('windowHeight')) {
+      if (index % 5 > 0 || index % 2 === 0) {
+        //66.67% of the flakes
+        this.repositionParticle(particle, random() * this.get('windowWidth'), -10, Math.floor(random() * 10) - 10);
+      } else {
+        if (Math.sin(this.get('angle')) > 0) {
+          // enter from the left
+          this.repositionParticle(particle, -5, random() * this.get('windowHeight'), Math.floor(random() * 10) - 10);
+        } else {
+          // enter from the right
+          this.repositionParticle(particle, this.get('windowWidth') + 5, random() * this.get('windowHeight'), Math.floor(random() * 10) - 10);
+        }
+      }
+    }
+  },
+
+  stepParticle(particle, particleIndex) {
+    particle.tiltAngle += particle.tiltAngleIncremental;
+    particle.y += (Math.cos(this.get('angle') + particle.d) + 3 + particle.r / 2) / 2;
+    particle.x += Math.sin(this.get('angle'));
+    particle.tilt = (Math.sin(particle.tiltAngle - (particleIndex / 3))) * 15;
+  },
+
+  repositionParticle(particle, xCoordinate, yCoordinate, tilt) {
+    particle.x = xCoordinate;
+    particle.y = yCoordinate;
+    particle.tilt = tilt;
+  },
+
+  animationLoop() {
+    if (this.isDestroyed || this.isDestroying) {
+      return;
+    }
+
+    raf(Ember.run.bind(this, 'animationLoop'));
+
+    return this.draw();
+  }
+});
